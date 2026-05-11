a1e5bd58991f44103061036d397bf7ae1d2a916c
25656 <25656@students.mrgs.school.nz>
2026-05-12 09:54:00 +1200

    Started to rewrite everything using ctk / polishing and commenting code

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import customtkinter as ctk
 import tkinter as tk
 import random
 
-class GermanApp:
+# ********** APP SETUP **********
 
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
+
+
+class GermanApp:
     def __init__(self,root):
         self.root=root
         self.root.title("German Guide for Kiwi Students")
-        self.root.configure(bg="#f1f5f8")
         self.root.geometry("700x600")
+        self.root.configure(bg="#f1f5f8")
 
-        #LESSON DATA
+        # ********** LESSON DATA ***********
+
         self.lesson_data = {
             "1": {
                 "name": "Lesson 1: Basics",
@@ -28,102 +35,131 @@ class GermanApp:
                 }
             }
         }
+
+        # ********** VARIABLES **********
+
         self.current_vocab = None
         self.words = []
         self.current_word = None
         self.score = 0
         self.index = 0
 
+        # ********** FONTS **********
 
         self.title_font_1 = ("Helvetica", 30, "bold")
         self.title_font_2 = ("Helvetica", 19, "bold")
         self.button_font = ("Helvetica", 15, "bold")
         self.text_font = ("Helvetica", 13)
 
-        self.button_img = tk.PhotoImage(file="button_normal.png")
-        self.button_hover_img = tk.PhotoImage(file="button_hover.png")
-        self.button_quit_img = tk.PhotoImage(file="button_quit.png")
-        self.button_quit_hover_img = tk.PhotoImage(file="button_quit_hover.png")
-
         self.create_home_screen()
 
-    #UI STYLING
+    # ********** CLEAR SCREEN ***********
+
     def clear_screen(self):
         for widget in self.root.winfo_children():
             widget.destroy()
 
-    def create_button(self, text, command):
-        btn = tk.Label(
-            self.root,
-            image=self.button_img,
-            text=text,
-            compound="center",
-            font=self.button_font,
-            fg="white",
-            bd=0
-        )
-        btn.bind("<Button-1>", lambda e: command())
+    # ********** CUSTOM BUTTON **********
 
-        btn.bind("<Enter>", lambda e: btn.config(image=self.button_hover_img))
-        btn.bind("<Leave>", lambda e: btn.config(image=self.button_img))
+    def create_button(self, text, command, quit_button=False):
+
+        if quit_button:
+            fg_color = "#ffffff"
+            hover_color = "#e8eef7"
+            text_color = "#1963cf"
+            width = 110
+        else:
+            fg_color = "#1963cf"
+            hover_color = "#1453af"
+            text_color = "white"
+            width = 260
+
+          btn = ctk.CTkButton(
+              self.root,
+              text = text,
+              command = command,
+              width = width,
+              height = 55,
+              corner_radius = 10,
+              font = self.button_font,
+              fg_color = fg_color,
+              hover_color = hover_color,
+              text_color = text_color,
+              border_width = 0
+          )
 
         return btn
 
-    #HOME SCREEN
+    # ********** HOME SCREEN **********
+
     def create_home_screen(self):
+
         self.clear_screen()
 
-        #Title
-        tk.Label(
-            self.root,
-            text="German Guide\nfor Kiwi Students",
-            font=self.title_font_1,
-            fg="#324366",
-            bg="#f1f5f8",
-            justify="center"
-        ).pack(pady=(50,30))
+        # Title
 
-        #Buttons
+        title = tk.Label(
+            self.root,
+            text = "German Guide\nfor Kiwi Students",
+            font = self.title_font_1,
+            fg = "#324366",
+            justify = "center"
+        )
+
+        title.pack(pady=(50,30))
+
+        # Lesson Buttons
+
         for key, lesson in self.lesson_data.items():
-            btn = self.create_button(lesson["name"], lambda k=key: self.start_lesson(k))
+
+            btn = self.create_button(
+                lesson["name"],
+                lambda k=key: self.start_lesson(k)
+            )
+
             btn.pack(pady=10)
 
-        #Subtitle
-        tk.Label(
-            self.root,
-            text="Master essential German fast!",
-            font=self.text_font,
-            bg="#f1f5f8",
-            fg="#324366"
-        ).pack(pady=(10,20))
+        # Subtitle
 
-        #Quit Button
-        self.quit_btn = tk.Label(
+        subtitle = tk.Label(
             self.root,
-            image=self.button_quit_img,
-            text="Quit",
-            compound="center",
-            font=self.button_font,
-            fg="#1963cf",
+            text = "Master essential German fast!",
+            font = self.text_font,
+            fg = "#324366"
         )
-        self.quit_btn.bind("<Button-1>", lambda e: self.root.destroy())
 
-        self.quit_btn.bind("<Enter>", lambda e: self.quit_btn.config(image=self.button_quit_hover_img))
-        self.quit_btn.bind("<Leave>", lambda e: self.quit_btn.config(image=self.button_quit_img))
+        subtitle.pack(pady=(15,25))
 
-        self.quit_btn.pack(pady=0)
+        # Quit Button
+
+        quit_btn = self.create_button(
+            "Quit",
+            self.root.destroy,
+            quit_button=True
+        )
+
+        quit_btn.pack()
 
 
-    #LESSON
+    # ********** LESSON **********
+
     def start_lesson(self, key):
+
         self.clear_screen()
+
         self.current_lesson_key = key
         self.current_vocab = self.lesson_data[key]["vocab"]
+
         self.words = list(self.current_vocab.items())
+
         self.index = 0
+
         self.display_word()
 
+    # ********** DISPLAY WORD **********
+
     def display_word(self):
+
         self.clear_screen()
 
         en, de = self.words[self.index]
